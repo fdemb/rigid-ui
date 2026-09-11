f3d75a8de3df421b4f869889a844171fef832d11
Filip Dembicki <69842744+fdemb@users.noreply.github.com>
2026-09-11 05:28:36 +0000

    fix(progress): add data attribute enums

---
+++ packages/rigid-ui/src/progress/root/ProgressRootDataAttributes.ts
@@ -0,0 +1,8 @@
+export enum ProgressRootDataAttributes {
+  /** Present when the progress has completed. */
+  complete = "data-complete",
+  /** Present when the progress is indeterminate. */
+  indeterminate = "data-indeterminate",
+  /** Present while the task is progressing. */
+  progressing = "data-progressing",
+}
